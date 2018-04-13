a92ac3f7cc30ffbd5d91c6c77e2ab901c6c42477
MysteriousCoyote <eldar.aliyev87@gmail.com>
2018-04-13 18:10:38 +0200

    added comments to function

--- list_files.py
+++ list_files.py
@@ -2,13 +2,19 @@
 #Too much files may overload
 import os
 
+#get currenr work directory
 start_point = os.getcwd()
 
 def list_files(_dir):
+    #list directories under current
     dirs = os.listdir(_dir)
     for dir in dirs:
+        #get path of next directory
         next_dir = os.path.join(_dir,dir)
+        #if current one is not direcotry - print the file name
         if not os.path.isdir(next_dir):
             print(dir + ' is file not dir !!! ')
         else:
+            #if current one is directory call myself
+            #print(next_dir)
             list_files(next_dir)
